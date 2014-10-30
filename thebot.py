b196cb3d26cefdbb74941813c454495d2048fbcc
qasimchadhar <qasimchadhar@users.noreply.github.com>
2014-10-30 06:46:24 -0500

    Updated to ignore already found posts

--- thebot.py
+++ thebot.py
@@ -1,24 +1,67 @@
+#!/usr/bin/python
+
+"""
+This bot searches a subreddit for specified term and sends an email notificaiton to defined address.
+Use at your own risk. No warranty ^_^
+Got suggessions? Have it your way :)
+"""
+
 import praw
-import sys
-import random
+import time
+import datetime
+import os
+import smtplib
+import email.utils
+from email.mime.text import MIMEText
 
-user_agent_name = 'myReditbot/1.0 by qasimchadhar' + str(random.randint(1, 20000000))
-print(user_agent_name)
 
-r = praw.Reddit(user_agent=user_agent_name)
+#Lets define the agent
+agent='Your unique bot agent' + str(datetime.datetime.now()) #and of course adding time to make it more unique
+r = praw.Reddit(user_agent=agent)
 
-# subreddit = r.get_subreddit("learnpython")
-# print("Printing top 10 submissions in /learnpython")
-# for submission in subreddit.get_hot(limit=10):
-#         # Test if it contains a PRAW-related question
-#     print(submission)
+#Lets define our variables
+postids=[]                                       #Bot will not search through posts it's already found results in
+toaddr = 'email@something.com'                # Change this
+fromaddr = 'email@something.com'                   # Change this
+subname = 'subreddit name'                      #without /r/
+searchfor = 'your keyword'
 
-# username = 'qasimchadhar'
+while True:
 
-username = sys.argv[1:]
-for each_user in username:
-    print("\n Comments found for " + each_user)
-    u = r.get_redditor(each_user)
-    for comment in u.get_comments(limit=10):
-        print(comment.body)
+    print("[+] Searching again \n \n \n")
+    results = r.search(searchfor, subreddit=subname, limit=0, sort='new')
 
+
+    for post in results:
+        if post.id in open("postids.txt",'r').read():
+       #if post.id in postids:  #If we have already looked at this post, ignore it
+            print "Post previously found."
+            break
+        else:
+            print(post.title)
+            print(post.url)
+            postids.append(post.id)
+            with open("postids.txt","a") as botfile:
+                botfile.write(post.id)
+                botfile.close()
+
+            title = post.title
+            url = post.url
+
+            #lest create the message
+            msg = MIMEText('Post title: ' + title + '\n Post URL: ' + url)
+            msg['To'] = email.utils.formataddr(('Recipient', toaddr))
+            msg['From'] = email.utils.formataddr(('RedditBot', fromaddr))
+            msg['Subject'] = title
+
+
+
+            server = smtplib.SMTP('localhost')                           #Change this
+            server.set_debuglevel(False) # show communication with the server. Change it to disable smtp debug log.
+            try:
+                server.sendmail(fromaddr, [toaddr], msg.as_string())
+                print "Email sent"
+            finally:
+                server.quit()
+
+    time.sleep(1800)              #Sleep for 1800 seconds or 30 minutes and search again
